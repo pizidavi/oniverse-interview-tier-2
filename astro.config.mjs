c8701310e704476870a52e88c43905094dcdd7f5
pizidavi <49226282+pizidavi@users.noreply.github.com>
2025-08-17 16:19:18 +0200

    ci: added build workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
+/* eslint-disable */
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import typedLinks from 'astro-typed-links';
+import { loadEnv } from 'vite';
+
+const { SITE_URL, BASE_URL } = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
+  base: BASE_URL,
   integrations: [typedLinks()],
   vite: {
     plugins: [tailwindcss()],
