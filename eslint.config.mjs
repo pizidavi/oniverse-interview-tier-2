2021842c6ef6aaef9a6d34f0982030663b408b7d
pizidavi <49226282+pizidavi@users.noreply.github.com>
2025-08-24 18:43:27 +0200

    feat: added technical data section

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,6 +36,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unnecessary-condition': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
